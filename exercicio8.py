a6f61dfde00c77345476c7f5b505d5d07a95882f
Paulo Victor <paulo.victorbn137@gmail.com>
2025-02-05 14:57:20 -0300

    exercicio de iterar strings com while

---
+++ exercicio8.py
@@ -0,0 +1,14 @@
+"""
+Iterando strings com while
+"""
+
+nome = 'Paulo Victor'
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += letra
+    indice += 1
+
+print(novo_nome)
+   
